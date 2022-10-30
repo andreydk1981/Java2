9e53f591449a3b8875c084d3df5b97a5c994392b
andreydk <0765871@gmail.com>
2022-10-30 23:42:15 +0600

    New method, no magic numbers

--- Lesson_2_HW/App.java
+++ Lesson_2_HW/App.java
@@ -3,6 +3,8 @@ package Lesson_2_HW;
 import java.util.Arrays;
 
 public class App {
+    private static final int LENGTH = 4;
+    private static boolean ERROR = false;
 
     public static void main(String[] args) {
         String[][] arr = new String[][]{
@@ -15,17 +17,16 @@ public class App {
         try {
             System.out.println("Sum numbs of Array = " + convertArray(arr));
         } catch (MyArraySizeException | MyArrayDataException e) {
+            ERROR = true;
             System.out.println("Handle: " + e.getMessage());
+        } finally {
+            System.out.println(String.format("Program finished %s", ERROR ? "with ERROR" : "OK"));
         }
 
     }
 
-    static int convertArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        if (arr.length != 4) throw new MyArraySizeException(String.format("Error size column Array[%d][]", arr.length));
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4)
-                throw new MyArraySizeException(String.format("Error Array[%d][%d] length", i, arr[i].length));
-        }
+    static int convertArray(String[][] arr) throws MyArrayDataException, MyArraySizeException {
+        checkArrLength(arr);
         System.out.println("Size of Array is OK");
         System.out.println(Arrays.deepToString(arr));
         System.out.println("Start check sum");
@@ -35,13 +36,22 @@ public class App {
             for (int j = 0; j < arr[i].length; j++) {
                 try {
                     num = Integer.parseInt(arr[i][j]);
-                    sum +=num;
+                    sum += num;
                 } catch (NumberFormatException exception) {
+                    ERROR = true;
                     throw new MyArrayDataException(String.format("Not a num in Arr[%d][%d]", i, j));
                 }
             }
-
         }
         return sum;
+    }
+
+    static void checkArrLength(String[][] arr) throws MyArraySizeException {
+        if (arr.length != LENGTH)
+            throw new MyArraySizeException(String.format("Error size column Array[%d][]", arr.length));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != LENGTH)
+                throw new MyArraySizeException(String.format("Error Array[%d][%d] length", i, arr[i].length));
+        }
     }
 }
